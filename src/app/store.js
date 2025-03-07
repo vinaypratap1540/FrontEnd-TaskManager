7f939654eaea5e1383a29f85ceb55f89757693af
Vinay Pratap Singh Sirohiya <123262231+vinaypratap1540@users.noreply.github.com>
2025-03-07 16:28:50 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer.js";
 import { authApi } from "../features/api/authApi.js";
+import { userLoggedIn } from "../features/authSlice.js"; 
 
 export const appStore = configureStore({
-    reducer:rootReducer,
-    middleware:(getDefaultMiddleware)=> getDefaultMiddleware().concat(authApi.middleware)
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
 });
 
 const initializeApp = async () => {
-    const { data } = await appStore.dispatch(authApi.endpoints.getUserProfile.initiate());
-    if (data) {
-        appStore.dispatch(userLoggedIn({ user: data.user })); //Ensure user is stored
+    try {
+        const { data } = await appStore.dispatch(authApi.endpoints.getUserProfile.initiate());
+        if (data && data.user) {
+            appStore.dispatch(userLoggedIn({ user: data.user })); // Dispatch userLoggedIn
+        }
+    } catch (error) {
+        console.error("Error initializing app:", error);
     }
 };
 initializeApp();
+
